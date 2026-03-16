7a12fe07c3f2027e4daff433006598859125d0a0
rohitranjan-2702 <rohitranjan2702@gmail.com>
2026-03-16 15:41:01 +0530

    feat: feedbucket

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          id="feedbucket"
+          strategy="afterInteractive"
+          data-feedbucket="PcuM73bWVVqTrDPJqi6z"
+          src="https://cdn.feedbucket.app/assets/feedbucket.js"
+        />
       </body>
     </html>
   );
